e3f8fe9982af9e8f7ab47cc86b34e90bccc4ffc0
JadeMorel <moreljade71@gmail.com>
2024-03-28 23:17:58 +0100

    Add Daddy Cena + Will

--- Assets/Scripts/Script_Score.cs
+++ Assets/Scripts/Script_Score.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 
 public class Script_Score : MonoBehaviour
 {
-    public int Score;
+    public static int Score;
     [SerializeField]   TextMeshProUGUI textScore;
 
     // Update is called once per frame
     void Update()
     { 
+
     }
+
+  
 
     /*void ScoreFinal() // donner des medaille ou qqc suivant le score je vous laisse imaginer les  recompences
     {
