79ba26834d9a0706b1878cf1e8b586a1acf9b86d
Ne0llix <william.darmon7@gmail.com>
2024-03-29 00:01:23 +0100

    derniers trucs

--- Assets/Scripts/Script_New_Ray.cs
+++ Assets/Scripts/Script_New_Ray.cs
@@ -36,7 +36,6 @@ public class Script_New_Ray : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
--- Assets/Scripts/Script_Score.cs
+++ Assets/Scripts/Script_Score.cs
@@ -6,14 +6,19 @@ using UnityEngine.UI;
 
 public class Script_Score : MonoBehaviour
 {
-    public int Score;
-    [SerializeField]   TextMeshProUGUI textScore;
+    public Script_New_Ray newRay;
+    [SerializeField] TextMeshProUGUI textScore;
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        Scoring();
     }
 
+    private void Scoring()
+    {
+        textScore.text = newRay.Score.ToString();
+    }
     /*void ScoreFinal() // donner des medaille ou qqc suivant le score je vous laisse imaginer les  recompences
     {
         if (Score == 0)
